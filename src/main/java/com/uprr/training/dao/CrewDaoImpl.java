73014efdfdef912225a157a04dd92cc02e80ea7d
David Zemon <david@stlswedespeed.com>
2014-04-09 16:14:40 -0500

    Seems to be getting closer; Need to correct JDBC connection URL

--- src/main/java/com/uprr/training/dao/CrewDaoImpl.java
+++ src/main/java/com/uprr/training/dao/CrewDaoImpl.java
@@ -1,6 +1,8 @@
 package com.uprr.training.dao;
 
 import com.uprr.training.pojos.CrewMember;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,8 +11,10 @@ import java.util.List;
 /**
  * @author David Zemon (dczemon)
  */
+@SuppressWarnings("unchecked")
 public class CrewDaoImpl implements CrewDao {
     private static List<CrewMember> staticCrewList;
+
     static {
         staticCrewList = new ArrayList<CrewMember>();
         staticCrewList.add(new CrewMember(0, "David", "Zemon", "6363950126"));
@@ -18,8 +22,15 @@ public class CrewDaoImpl implements CrewDao {
         staticCrewList.add(new CrewMember(2, "Carl", "Brooks", "4025442919"));
     }
 
+    private SessionFactory sf;
+
+    public CrewDaoImpl(SessionFactory sf) {
+        this.sf = sf;
+    }
+
     /**
      * Get all available crew members for a given date
+     *
      * @param date Date that crew should be available
      * @return List of crew members; Null if none available
      */
@@ -31,11 +42,12 @@ public class CrewDaoImpl implements CrewDao {
 
     /**
      * Retrieve a list of all active crew members
+     *
      * @return List of crew members in the database
      */
     @Override
     public List<CrewMember> getAllCrew() {
-        // TODO: Add real database access
-        return CrewDaoImpl.staticCrewList;
+
+        return (List<CrewMember>) sf.getCurrentSession().createQuery("").list();
     }
 }
---
+++ src/test/java/com/uprr/training/dao/CrewDaoImplTest.java
@@ -0,0 +1,23 @@
+package com.uprr.training.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by igen006 on 4/9/2014.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@Transactional
+public class CrewDaoImplTest {
+    @Autowired
+    private CrewDao crewDao;
+
+    @Test
+    public void testGetAllCrew() throws Exception {
+    }
+}

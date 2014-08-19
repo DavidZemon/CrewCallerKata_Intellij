d9680f0e67cc4587975d4e883457734d4f043b3b
David Zemon <david@zemon.name>
2014-08-19 09:24:38 -0500

    Clean code fixes; Reformat some XML

--- src/test/java/com/uprr/training/dao/CrewDaoImplTest.java
+++ src/test/java/com/uprr/training/dao/CrewDaoImplTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.assertEquals;
  * Created by igen006 on 4/9/2014.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring/application-context.xml"})
+@ContextConfiguration(locations = {"classpath:spring/CrewCaller-context.xml"})
 @Transactional
 public class CrewDaoImplTest {
     @Autowired
